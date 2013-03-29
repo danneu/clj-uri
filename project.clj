0964a8fe4dcd2f6cbff925510ad09f304512c983
Dan Neumann <danrodneu@gmail.com>
2013-03-28 22:58:07 -0500

    add license

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clj-uri "0.1.0-SNAPSHOT"
   :description "Parse URI strings into URI components. (wraps java.net.URI)"
   :url "http://github.com/danneu/clj-uri"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.6.4"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
   :dependencies [[org.clojure/clojure "1.4.0"]])
